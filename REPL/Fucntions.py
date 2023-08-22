24c57176778d9787ef9fcce3dc39df474e9521a7
piyushzope15 <piyush_zope@persistent.com>
2023-08-22 15:37:08 +0530

    REPL

---
+++ REPL/Fucntions.py
@@ -0,0 +1,13 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> numbers = [2,4,1,3]
+>>> numbers
+[2, 4, 1, 3]
+>>> print(numbers.append(6))
+None
+>>> pow(4,2)
+16
+>>> len(numbers)
+5
+>>> sum(numbers)
+16

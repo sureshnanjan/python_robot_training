c74cc0e9f161fd2ea97d20640e407057a1c81498
piyushzope15 <piyush_zope@persistent.com>
2023-08-22 15:43:14 +0530

    REPL

---
+++ REPL/importing.py
@@ -0,0 +1,9 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import os
+>>> os.chdir("D:\SSP Project Training\Robot training\Robot Training Repo\python_robot_training\REPL")
+>>> import sum
+>>> sum.sum()
+Type a number: 10
+Type another number: 10
+The sum is:  20
---
+++ REPL/sum.py
@@ -0,0 +1,7 @@
+def sum():
+    x = input("Type a number: ")
+    y = input("Type another number: ")
+
+    sum = int(x) + int(y)
+
+    print("The sum is: ", sum)
